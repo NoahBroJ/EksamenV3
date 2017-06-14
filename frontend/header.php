2b5466f6be113ce800fea5d2c6956fdb990b70ef
NoahBroJ <noahzarkbro@gmail.com>
2017-06-14 14:36:00 +0200

    Established basic file structure

---
+++ frontend/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!--Meta tags and SEO-->
+    <meta charset="UTF-8">
+    <meta title="<?php echo $title ?>">
+    <meta name="description" content="<?php echo $description ?>">
+    <meta name="keywords" content="">
+    <meta name="author" content="Noah Bro-Jørgensen">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $pageTitle ?></title>
+    
+    <!--Favicon-->
+    <link rel="icon" type="image/png" href="">
+    
+    <!--Link style sheets-->
+    <link rel="stylesheet" href="../styles/master.css">
+    <link rel="stylesheet" href="../styles/<?php echo $page ?>.css">
+    
+    <!--Link JQuery, Font Awesome, and script-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://use.fontawesome.com/d333651ce6.js"></script>
+    <script src="../scripts/<?php echo $page ?>.js"></script>
+    
+</head>
+<body>
+    <header>
+        
+    </header>
---
+++ frontend/index.php
@@ -0,0 +1,19 @@
+<?php
+    /*Declare page variables*/
+    $page = "index";
+    $title = "";
+    $description = "";
+    $pageTitle = "";
+    
+    /*Include head and header*/
+    require "header.php";
+?>
+
+<main>
+    
+</main>
+
+<?php
+    /*Include footer*/
+    require "footer.php";
+?>
